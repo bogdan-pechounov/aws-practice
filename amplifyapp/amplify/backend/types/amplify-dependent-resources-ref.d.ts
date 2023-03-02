04d641321a7d9dddb1e8333412162053c010e6ae
Bogdan <bogdan.pechounov@gmail.com>
2023-03-01 22:39:52 -0500

    Added graphql and storage

--- amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s35c38db28": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
